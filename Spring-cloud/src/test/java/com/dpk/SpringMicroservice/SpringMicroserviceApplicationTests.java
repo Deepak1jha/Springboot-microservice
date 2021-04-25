d48e57f1b3f56d55993d469b638aea885bfe5e00
Deepak1jha <ddjha171@gmail.com>
2021-04-25 12:06:15 +0530

    first commit

---
+++ Spring-cloud/src/test/java/com/dpk/SpringMicroservice/SpringMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpk.SpringMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
